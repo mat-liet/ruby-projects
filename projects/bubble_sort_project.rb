8220f516d5a6ab10600c7db29a2be2dac044adc4
mat-liet <matejlietava@gmail.com>
2021-07-04 22:15:13 +0100

    Added bubble sort project

---
+++ projects/bubble_sort_project.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class BubbleSort
+
+    def bubble_sort(array)
+        index = 0
+        
+
+        if num > array[index + 1]
+            array[index], array[index + 1] = array[index + 1], array[index]
+        end
+    end
+end
--- projects/stockpicker_project.rb
+++ projects/stockpicker_project.rb
@@ -2,7 +2,19 @@ require 'pry'
 
 class StockPicker
 
+    # ary.each_with_index.min  # => [1, 4]
+    # first is value and second is index
     def pickstock(array)
-        
+        min_val_index = array.each_with_index.min[1]
+        # puts min_val_index
+        highest_value_index = array.each_with_index.max[1]
+        # puts highest_value_index
+        until highest_value_index > min_val_index
+            array[highest_value_index + 1, array.length]
+        end
+        puts [min_val_index, highest_value_index]
     end
+
+    picker = StockPicker.new
+    picker.pickstock([17,3,6,9,15,8,6,1,10])
 end

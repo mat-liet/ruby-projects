64cf94c0f757bb10bde189954ebb9284c467e489
mat-liet <matejlietava@gmail.com>
2021-07-05 00:17:13 +0100

    Added few lines

--- projects/bubble_sort_project.rb
+++ projects/bubble_sort_project.rb
@@ -4,10 +4,13 @@ class BubbleSort
 
     def bubble_sort(array)
         index = 0
-        
-
-        if num > array[index + 1]
-            array[index], array[index + 1] = array[index + 1], array[index]
-        end
+        for i in array do
+            for j in array - i do
+                
+            end
+          end
+        # if num > array[index + 1]
+        #     array[index], array[index + 1] = array[index + 1], array[index]
+        # end
     end
 end

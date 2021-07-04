4b20afd0bf1c2109599de80437231dfe1b6beaea
mat-liet <matejlietava@gmail.com>
2021-07-04 18:43:30 +0100

    Started working on second challenge

---
+++ projects/substring_project.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+# Class that contains a method which takes a string and an array of strings and checks how many times
+# each word is in the string
+# author: Matej Lietava
+class SubstringProject
+    
+    def substrings(string, array_of_words)
+        result_hash = Hash.new
+        string.downcase!
+        array_of_words.each do |word|
+            temp_string = string
+            count = 0;
+            until !temp_string.include?(word)
+                if temp_string.include?(word) then count+= 1 end
+                index = temp_string.index(word)
+                temp_string = temp_string.slice(index + 1, temp_string.length - index)
+            end
+            if count > 0 then result_hash[word] = count end
+        end
+        result_hash
+    end
+
+    substring = SubstringProject.new
+    string  = "Howdy partner, sit down! How's it going?";
+    dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit", "below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    puts substring.substrings(string, dictionary)
+end
---
+++ spec/substring_project_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require_relative '../projects/substring_project'
+
+describe SubstringProject do
+
+    context "When testing the substrings method of the SubstringProject class" do
+        it "will return the hash containing the right counts of substrings of a valid string" do
+            substring = SubstringProject.new
+            
+        end
+    end
+end
